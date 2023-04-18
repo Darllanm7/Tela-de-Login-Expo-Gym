66a19bdbe278a29571300a7975730cbe3e3be0ab
Darllan Matos <118637732+Darllanm7@users.noreply.github.com>
2023-04-18 09:45:18 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons'; 
 
 export default function HomeScreen() {
   return (
@@ -23,8 +24,8 @@ export default function HomeScreen() {
           <Text style={styles.boxTitle}>Nutrição</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.box}>
-          <Ionicons name="calendar-outline" size={50} color="black" />
-          <Text style={styles.boxTitle}>Agenda</Text>
+         <AntDesign name="linechart" size={50} color="black" />
+          <Text style={styles.boxTitle}>AProgresso</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.box}>
           <Ionicons name="person-outline" size={50} color="black" />
